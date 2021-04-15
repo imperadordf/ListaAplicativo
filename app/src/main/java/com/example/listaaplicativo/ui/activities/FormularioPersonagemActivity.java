73b5bae05c398ab1bbb98faf2916557d5def901f
imperadordf <49728767+imperadordf@users.noreply.github.com>
2021-04-14 21:00:37 -0300

    Added activity_formulario_personagem

---
+++ app/src/main/java/com/example/listaaplicativo/ui/activities/FormularioPersonagemActivity.java
@@ -0,0 +1,14 @@
+package com.example.listaaplicativo.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormularioPersonagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_personagem);
+    }
+}
