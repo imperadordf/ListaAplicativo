4052041dcd860f8482e6242d31448afc9f9fd2c2
DESKTOP-M77IFTI\lucas <lucasdf300@gmail.com>
2021-04-26 16:18:36 -0300

    Added

--- app/src/main/java/com/example/listaaplicativo/dao/PersonagemDAO.java
+++ app/src/main/java/com/example/listaaplicativo/dao/PersonagemDAO.java
@@ -10,21 +10,27 @@ public class PersonagemDAO {
     private final static List<Personagem> personagens = new ArrayList<>();
     private static int contadorDeID= 1;
 
+    //Metodo que salva o Personagem que recebe no metodo
     public void salvar(Personagem personagemSalvo){
+        //Salvo o Personagem e dou o ID que ele esta no momento
         personagemSalvo.setId(contadorDeID);
+        //Adiciono ele na Lista
         personagens.add(personagemSalvo);
+        //Aumento o ID para o proximo Personagem, para cada personagem ter um ID diferente.
         contadorDeID++;
     }
 
     public void edita(Personagem personagem)
     {
+        //Crio um Personagem escolhido e ele vai buscar o Personagem pelo ID e referencia esse personagemEscolhido
         Personagem personagemEscolhido = buscaPersonagemId(personagem);
+        //Se o Personagem for diferente de Nulo, eu coloco o Personagem na Posição dele e mando o personagem
         if(personagemEscolhido!=null){
             int posicaoDoPersonagem = personagens.indexOf(personagemEscolhido);
             personagens.set(posicaoDoPersonagem,personagem);
         }
     }
-
+    //Procuro o Personagem pelo o ID Dele, verifico se na lista de personagem, existe algum personagem com o ID dele, caso exista, returno ele
     public  Personagem buscaPersonagemId(Personagem personagem)
     {
         for (Personagem p: personagens) {
@@ -35,10 +41,12 @@ public class PersonagemDAO {
         return  null;
     }
 
+    //um Metodo que Return a lista de Personagens que eu tenho
     public List<Personagem> todos(){
         return new ArrayList<>(personagens);
     }
 
+    //Removo o Personagem, buscando ele pelo ID e removendo  ele da Lista
     public void remove(Personagem personagem){
         Personagem personagemDevolvido = buscaPersonagemId(personagem);
         if(personagemDevolvido!=null){
--- app/src/main/java/com/example/listaaplicativo/ui/activities/FormularioPersonagemActivity.java
+++ app/src/main/java/com/example/listaaplicativo/ui/activities/FormularioPersonagemActivity.java
@@ -16,31 +16,43 @@ import com.example.listaaplicativo.model.Personagem;
 import static com.example.listaaplicativo.ui.activities.ConstantesActivities.CHAVE_PERSONAGEM;
 
 public class FormularioPersonagemActivity extends AppCompatActivity {
-
+    //String para caso for editar ou novo Personagem
     private static  final String TITUTLO_APPBAR_EDITA_PERSONAGEM ="EDITAR PERSONAGENS";
     private static  final String TITUTLO_APPBAR_NOVO_PERSONAGEM ="NOVO PERSONAGEM";
 
+    //Edit text para o Nome, altura e Nascimento, declarando eles para ser usado nos metodos
     private EditText campoNome;
     private EditText campoAltura;
     private EditText campoNascimento;
 
+    //Criando um PersonagemDAO, que ele tem a lista e configura os personagens
     private final PersonagemDAO dao = new PersonagemDAO();
+    //Uma classe Personagem, que vai ser criado ou editado
     private Personagem personagem;
 
+    //Seria o Start do Android Studio a onde tudo começa, o Main que seria a criação de tudo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setando o contexto que esse Script teria a logica do Layou activity_Formulario_personagem
         setContentView(R.layout.activity_formulario_personagem);
+        //Inicializa o campos
         inicializeCampos();
         configuraBotaoSalvar();
         carregarPersonagem();
+
+        //Estou dando os valores inicias e configurando tudo para o Usuario
     }
 
     private void carregarPersonagem() {
+        //Metodo que verifica se existe um Personagem salvo, caso seja um personagem Editado
         Intent dados = getIntent();
         if(dados.hasExtra(CHAVE_PERSONAGEM)){
+            //Caso seja um Personagem para Edita,eu mudo o titulo do aplicativo no topo.
             setTitle(TITUTLO_APPBAR_EDITA_PERSONAGEM);
+            //Pego a referencia do Personagem usando Serializable
             personagem = (Personagem) dados.getSerializableExtra(CHAVE_PERSONAGEM);
+            //Chamo um metodo que pega os dados do Usuario e coloca na tela do aplicativo
             preencherCampos();
         }else{
             setTitle(TITUTLO_APPBAR_NOVO_PERSONAGEM);
@@ -49,42 +61,51 @@ public class FormularioPersonagemActivity extends AppCompatActivity {
     }
 
     private void preencherCampos() {
+        //Estou dando os valores no campo para digitar, pegando os dados e colocando visualmente.
         campoNome.setText(personagem.getNome());
         campoAltura.setText(personagem.getAltura());
         campoNascimento.setText(personagem.getNascimento());
     }
 
     private void configuraBotaoSalvar() {
+        //Configuro o botão de Salvar e dou a Referencia
         Button botaoEnviar = findViewById(R.id.botaoEnviar);
+        // estou dando a logica que ação ele vai tomar ao clickar
         botaoEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
+            //Se for Clickando ele chama o FinalizaFormulario
             public void onClick(View v) {
                 FinalizaFormulario();
             }
         });
     }
-
+   //
     private void FinalizaFormulario() {
+        //Chamo o Metodo que seta os valores do meu Personagem
         preencherPersonagem();
+        //Verifico se estou editando ou Criando um personagem
         if(personagem.IdValide()){
             dao.edita(personagem);
         }else{
             dao.salvar(personagem);
         }
+        //Finalizo a criação do Personagem e volto para tela inicial
         finish();
     }
 
     private void preencherPersonagem() {
+        //Estou pegando as referencia dos nomes inseridos pelo Usuario.
       String nome = campoNome.getText().toString();
       String altura = campoAltura.getText().toString();
       String nascimento = campoAltura.getText().toString();
-
+      //Estou mandando para a minha classe Personagem para criar o Personagem, dando Nome, altura e Nascimento.
       personagem.setNome(nome);
       personagem.setAltura(altura);
       personagem.setNascimento(nascimento);
     }
 
     private void inicializeCampos() {
+        //dou valores para os meus EditTEXT/ a referencia de cada- POO
         campoNome = findViewById(R.id.editTextName);
         campoAltura = findViewById(R.id.editTextAltura);
         campoNascimento = findViewById(R.id.editTextNascimento);
